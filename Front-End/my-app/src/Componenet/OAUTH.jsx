a49bac4e3c85b24b2b320136c81b36034e9ff903
vijayakumar1069 <vijay.r20799@gmail.com>
2024-01-19 23:16:26 +0530

    final commits

--- Front-End/my-app/src/Componenet/OAUTH.jsx
+++ Front-End/my-app/src/Componenet/OAUTH.jsx
@@ -40,7 +40,7 @@
 //   );
 // }
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import { app } from '../firebase';
+import { app } from '../firebase.js';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
