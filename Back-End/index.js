7af349872b768e6325d1b016d68f18c9b37fb012
vijayakumar1069 <vijay.r20799@gmail.com>
2024-01-19 23:37:41 +0530

    add pre build

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,14 +5,16 @@ const bodyparser = require("body-parser");
 const dotenv = require("dotenv").config();
 const userRouter = require("./routes/user.router.js");
 const authRouter = require("./routes/auth.router.js");
-const listingRouter=require("./routes/listing.route.js")
-const cookieparser=require("cookie-parser")
-
+const listingRouter = require("./routes/listing.route.js");
+const path = require("path");
+const cookieparser = require("cookie-parser");
+const __dircname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use(express.json());
-app.use(cookieparser())
+app.use(cookieparser());
+
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
@@ -26,6 +28,12 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dircname, "/Front-End/my-app/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dircname, "Front-End", "my-app", "dist", "index.html")
+  );
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";
